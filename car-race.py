b6f125cd6bd21c463d4ecb2f47b66aea0d6062b6
rem <skyrem@qq.com>
2020-08-18 01:18:50 +0800

    create menu

--- car-race.py
+++ car-race.py
@@ -15,6 +15,10 @@ carImg = pygame.image.load('racecar.png')
 
 black = (0, 0, 0)
 white = (255, 255, 255)
+red = (200,0,0)
+green = (0,200,0)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
 
 
 def things(thingx, thingy, thingw, thingh, color):
@@ -36,6 +40,46 @@ def things_dodged(count):
     font = pygame.font.SysFont(None, 25)
     text = font.render("Dodged: "+str(count), True, black)
     gameDisplay.blit(text,(0,0))
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+    if click[0] == 1 and action != None:
+        action()         
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.Font("freesansbold.ttf",20)
+    textSurf = smallText.render(msg, False,(0,0,0))
+    textRect = textSurf.get_rect()
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+
+def game_intro():
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf',115)
+        textSurf = largeText.render("A bit Racey",False,(0,0,0))
+        textRect = textSurf.get_rect()
+        textRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(textSurf, textRect)
+
+        button("GO!",150,450,100,50,green,bright_green,gameLoop)
+        button("Quit",550,450,100,50,red,bright_red,quitGame)
+
+def quitGame():
+    message_display('crashed')
+    gameOver = True
+    pygame.quit()
+
 
 def gameLoop():
     gameOver = False
@@ -93,6 +137,5 @@ def gameLoop():
         pygame.display.update()
         clock.tick(60)
 
- 
+game_intro()
 gameLoop()
-pygame.quit()
